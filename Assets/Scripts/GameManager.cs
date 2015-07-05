30451965278d9c1c5e033e8f2ec69698dfe6c52a
Brennan Anderson <Brennan.Anderson@disney.com>
2015-07-04 21:40:51 -0700

    Added Music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Application.targetFrameRate = 60;
+		// Audio - Start Music
+		if (!Fabric.EventManager.Instance.IsEventActive("Music/Switch", gameObject)) {
+			Fabric.EventManager.Instance.PostEvent("Music/Switch");
+		}
 	}
 	
 	// Update is called once per frame
