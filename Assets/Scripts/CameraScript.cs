ec193b6609792c89fd9240913df26944952dfcff
stuas014 <Spencer.Stuard@disney.com>
2015-07-04 20:26:21 -0700

    1st Commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform BallRef;
+	public float UpOffset;
+	public float FrontOffset;
+
+	Vector3 MoveToLocation;
+	public float MoveSpeed;
+	public float DistanceToTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		if(BallRef)
+		{
+			//DISTANCE
+			DistanceToTarget = Vector3.Distance(transform.position, BallRef.transform.position);
+
+			//GET MOVE TO LOCATION
+			if(BallRef.GetComponent<Rigidbody>().velocity.normalized == Vector3.zero)
+			{
+				MoveToLocation = BallRef.position + new Vector3(0f,0f,-2.5f);
+			}
+			else
+			{
+				MoveToLocation = BallRef.position + ((BallRef.GetComponent<Rigidbody>().velocity.normalized) * -2.5f);
+			}
+			MoveToLocation += (Vector3.up * 2);
+
+			//MOVE
+			transform.position = Vector3.Lerp(transform.position, MoveToLocation, Time.deltaTime * (DistanceToTarget/3));
+
+			//LOOK AT
+			Vector3 relativePos = (BallRef.transform.position + Vector3.up) - transform.position;
+			Quaternion rotation = Quaternion.LookRotation(relativePos);
+			transform.rotation = rotation;
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float TorqueAmount;
+	public Rigidbody rb;
+	public float HorizontalForce;
+	public float VerticalForce;
+
+	Vector3 LastMousePos = Vector3.zero;
+	float XMouseDelta = 0f;
+	float YMouseDelta = 0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = transform.GetComponent<Rigidbody>();
+		rb.maxAngularVelocity = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//UsingKeyboardInput ();
+		UsingMouseInput ();
+
+		if(Input.GetMouseButton(0))
+		{
+			rb.AddTorque(Camera.main.transform.right * VerticalForce);
+			rb.AddTorque(Camera.main.transform.forward * HorizontalForce);
+
+			/*
+			rb.AddTorque(Vector3.right * VerticalForce);
+			rb.AddTorque(Vector3.forward * HorizontalForce);
+			*/
+		}
+
+		//Debug.Log(rb.velocity);
+	
+	}
+
+	void UsingKeyboardInput ()
+	{
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			VerticalForce = 1;
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			VerticalForce = -1;
+		}
+		else
+		{
+			VerticalForce = 0;
+		}
+		
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			HorizontalForce = -1;
+		}
+		else if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			HorizontalForce = 1;
+		}
+		else
+		{
+			HorizontalForce = 0;
+		}
+	}
+
+	void UsingMouseInput ()
+	{
+		//Debug.Log(Input.mousePosition);
+
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			LastMousePos = Input.mousePosition;
+		}
+
+		if(Input.GetMouseButton(0))
+		{
+			XMouseDelta = Input.mousePosition.x - LastMousePos.x;
+			YMouseDelta = Input.mousePosition.y - LastMousePos.y;
+
+			HorizontalForce = -1 * XMouseDelta * TorqueAmount;
+			VerticalForce = YMouseDelta * TorqueAmount;
+		}
+		LastMousePos = Input.mousePosition;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Reset")
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+
+}
